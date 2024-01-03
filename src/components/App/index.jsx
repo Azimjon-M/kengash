579c6068cf8b8f424197521be49c6f4b6dbb9eb2
Azimjon-M <azimeliboev2000@gmail.com>
2024-01-03 14:59:29 +0500

    Default Taliwind router-dom redux...

---
+++ src/components/App/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <button className="btn btn-neutral">App</button>
+            <button className="btn btn-accent">Accent</button>
+        </div>
+    )
+}
+
+export default App;
