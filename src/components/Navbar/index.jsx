e7991da81aa402d736f0d09c58458e1785fd94d1
Azimjon-M <azimeliboev2000@gmail.com>
2024-01-03 16:09:55 +0500

    Navbar ++

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='border border-red-600'>
+            <div>
+                <div>
+                    {/* <img src={} alt="image" /> */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Navbar from '../../components/Navbar';
 
 const Main = () => {
     return (
-        <div>Main</div>
+        <div>
+            <Navbar />
+        </div>
     )
 }
 
