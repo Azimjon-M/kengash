2430ac4bad2a6e94d8bc4dfbc2801d39cf5e6ee3
Azimjon-M <azimeliboev2000@gmail.com>
2024-01-04 12:50:59 +0500

    Navbar ++ mobile drop +

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,50 +1,166 @@
-import React, { useEffect, useState } from 'react';
-import kspiIcon from '../../assets/icons/logo_kspi.png'
-import { Link } from 'react-router-dom';
-import { FiMenu } from 'react-icons/fi'
+import React, { useEffect, useState } from "react";
+import kspiIcon from "../../assets/icons/logo_kspi.png";
+import { Link, NavLink } from "react-router-dom";
+import { FiMenu } from "react-icons/fi";
 
-const Navbar = ({isExit}) => {
+// isExit 
+const Navbar = ({ outside }) => {
     // Values
-    const [isExitOrAccess, setIsExitOrAccess] = useState('KIRISH')
-    const [isOpenMeni, setIsOpenMeni] = useState(false)
+    const [isExitOrAccess, setIsExitOrAccess] = useState("KIRISH");
+    const [isOpenMeni, setIsOpenMeni] = useState(false);
     // Funtions
     useEffect(() => {
-        if (isExit) {
-            setIsExitOrAccess('CHIQISH');
+        if (outside) {
+            setIsExitOrAccess("KIRISH");
         } else {
-            setIsExitOrAccess('KIRISH');
+            setIsExitOrAccess("CHIQISH");
         }
-    }, [isExit])
+    }, [outside]);
 
     return (
-        <div className='flex flex-col'>
-            <div className='flex justify-between items-center bg-white text-black px-6 py-3'>
+        <div className="flex flex-col">
+            <div className="flex justify-between items-center bg-white text-black px-6 py-3">
                 <Link to="/">
-                    <div className='flex justify-start items-center gap-x-3'>
-                        <div className='lg:hidden'>
-                            <FiMenu onClick={() => setIsOpenMeni(!isOpenMeni)} className='text-[30px]' />
+                    <div className="flex justify-start items-center">
+                        <div className={`${outside ? "hidden" : " lg:hidden"} `}>
+                            <FiMenu
+                                onClick={() => setIsOpenMeni(!isOpenMeni)}
+                                className="text-[30px]"
+                            />
                         </div>
-                        <div className='w-[40px] h-auto hidden lg:inline-block'>
-                            <img className='w-full h-auto' src={kspiIcon} alt="KSPI icon" />
+                        <div className={`${outside ? "flex" : "hidden lg:flex "} items-center gap-x-3`}>
+                            <div className="w-[40px] h-auto">
+                                <img
+                                    className="w-full h-auto"
+                                    src={kspiIcon}
+                                    alt="KSPI icon"
+                                />
+                            </div>
+                            <h1 className="font-bold text-[20px]">
+                                QDPI Kengashi
+                            </h1>
                         </div>
-                        <h1 className='font-bold text-[20px] hidden lg:inline-block'>
-                            QDPI Kengashi
-                        </h1>
                     </div>
                 </Link>
-                <div>
-                    <button className='text-[16px] btn btn-success bg-[#05B967] font-medium text-white px-8'>
+                <div className="flex items-center gap-x-20">
+                    <ul className={`${outside ? "hidden" : "hidden lg:flex lg:gap-x-3 font-semibold"}`}>
+                        <li>
+                            <NavLink
+                                to="/"
+                                className={(isActive) =>
+                                    `${isActive ? "underline" : ""}`
+                                }
+                            >
+                                Asosiy
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/dwas"
+                                className={(isActive) =>
+                                    `${isActive ? "underline" : ""}`
+                                }
+                            >
+                                A'zolar
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/wdas"
+                                className={(isActive) =>
+                                    `${isActive ? "underline" : ""}`
+                                }
+                            >
+                                Davomat
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/sdlapdlsap"
+                                className={(isActive) =>
+                                    `${isActive ? "underline" : ""}`
+                                }
+                            >
+                                Takliflar
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/adsaiki"
+                                className={(isActive) =>
+                                    `${isActive ? "underline" : ""}`
+                                }
+                            >
+                                Statistika
+                            </NavLink>
+                        </li>
+                    </ul>
+                    <button className="text-[16px] btn btn-success bg-[#05B967] font-medium text-white px-8">
                         {isExitOrAccess}
                     </button>
                 </div>
             </div>
 
-            <div className={`${isOpenMeni ? "h-[0px]" : "h-auto"} style-transition border border-red-600 `}>
-                hellos <br />
-                lksdk
+            <div
+                className={` style-transition overflow-hidden ${
+                    isOpenMeni ? "h-[200px] py-2" : "h-[0px]"
+                } bg-white`}
+            >
+                <ul className="flex flex-col items-center gap-y-3 font-semibold text-black">
+                    <li>
+                        <NavLink
+                            to="/"
+                            className={(isActive) =>
+                                `${isActive ? "underline" : ""}`
+                            }
+                        >
+                            Asosiy
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/dwas"
+                            className={(isActive) =>
+                                `${isActive ? "underline" : ""}`
+                            }
+                        >
+                            A'zolar
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/wdas"
+                            className={(isActive) =>
+                                `${isActive ? "underline" : ""}`
+                            }
+                        >
+                            Davomat
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/sdlapdlsap"
+                            className={(isActive) =>
+                                `${isActive ? "underline" : ""}`
+                            }
+                        >
+                            Takliflar
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/adsaiki"
+                            className={(isActive) =>
+                                `${isActive ? "underline" : ""}`
+                            }
+                        >
+                            Statistika
+                        </NavLink>
+                    </li>
+                </ul>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
