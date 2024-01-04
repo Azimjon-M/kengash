e32a4ebc9d9fcc78c2b9b69b56867ddfa94ed0eb
nodirbek27 <nodirjon0927@gmail.com>
2024-01-04 14:38:53 +0500

    login++

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import { routes } from '../../routes';
+import Login from '../Login';
 
 const App = () => {
     const content = useRoutes(routes);
     return (
         <div>
+            <Login />
             {content}
         </div>
     )
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -19,13 +19,13 @@ const Login = () => {
                         <label for="username" className="block text-sm font-semibold text-gray-800">
                             Foydalanuvchi nomi
                         </label>
-                        <input type="username" className="block w-full px-4 py-2 mt-2 text-black bg-white border rounded-md focus:border-[#28a745] focus:ring-[#25a620] focus:outline-none focus:ring focus:ring-opacity-40"/>
+                        <input type="username" placeholder='Foydalanuvchi nomi' className="block w-full px-4 py-2 mt-2 text-black bg-white border rounded-md focus:border-[#28a745] focus:ring-[#25a620] focus:outline-none focus:ring focus:ring-opacity-40"/>
                     </div>
                     <div className="mb-2">
                         <label for="password" className="block text-sm font-semibold text-gray-800">
                             Parol
                         </label>
-                        <input type="password" className="block w-full px-4 py-2 mt-2 text-black bg-white border rounded-md focus:border-[#28a745] focus:ring-[#25a620] focus:outline-none focus:ring focus:ring-opacity-40"/>
+                        <input type="password" placeholder='Parol' className="block w-full px-4 py-2 mt-2 text-black bg-white border rounded-md focus:border-[#28a745] focus:ring-[#25a620] focus:outline-none focus:ring focus:ring-opacity-40"/>
                     </div>
                     <div className="mt-6">
                         <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-[#28a745] rounded-md hover:bg-[#25a620] focus:outline-none">
