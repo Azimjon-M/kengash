4aac6ca62a8a68fe30eb57b9493d8447cecea992
Azimjon-M <azimeliboev2000@gmail.com>
2024-01-04 11:55:47 +0500

    app.css +, Navbar ++, Ract icons +

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useRoutes } from 'react-router-dom';
 import { routes } from '../../routes';
+import './style.css';
 
 const App = () => {
     const content = useRoutes(routes);
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import kspiIcon from '../../assets/icons/logo_kspi.png'
+import { Link } from 'react-router-dom';
+import { FiMenu } from 'react-icons/fi'
 
-const Navbar = () => {
+const Navbar = ({isExit}) => {
+    // Values
+    const [isExitOrAccess, setIsExitOrAccess] = useState('KIRISH')
+    const [isOpenMeni, setIsOpenMeni] = useState(false)
+    // Funtions
+    useEffect(() => {
+        if (isExit) {
+            setIsExitOrAccess('CHIQISH');
+        } else {
+            setIsExitOrAccess('KIRISH');
+        }
+    }, [isExit])
+
     return (
-        <div className='border border-red-600'>
-            <div>
+        <div className='flex flex-col'>
+            <div className='flex justify-between items-center bg-white text-black px-6 py-3'>
+                <Link to="/">
+                    <div className='flex justify-start items-center gap-x-3'>
+                        <div className='lg:hidden'>
+                            <FiMenu onClick={() => setIsOpenMeni(!isOpenMeni)} className='text-[30px]' />
+                        </div>
+                        <div className='w-[40px] h-auto hidden lg:inline-block'>
+                            <img className='w-full h-auto' src={kspiIcon} alt="KSPI icon" />
+                        </div>
+                        <h1 className='font-bold text-[20px] hidden lg:inline-block'>
+                            QDPI Kengashi
+                        </h1>
+                    </div>
+                </Link>
                 <div>
-                    <img src={kspiIcon} alt="KSPI icon" />
+                    <button className='text-[16px] btn btn-success bg-[#05B967] font-medium text-white px-8'>
+                        {isExitOrAccess}
+                    </button>
                 </div>
+            </div>
+
+            <div className={`${isOpenMeni ? "h-[0px]" : "h-auto"} style-transition border border-red-600 `}>
+                hellos <br />
+                lksdk
             </div>
         </div>
     )
