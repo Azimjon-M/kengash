61d7da0523899c785b5ff7c1018ee47c601acf2d
Azimjon-M <azimeliboev2000@gmail.com>
2024-01-04 10:56:08 +0500

    Navbar ++ icon+ image +

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import kspiIcon from '../../assets/icons/logo_kspi.png'
 
 const Navbar = () => {
     return (
         <div className='border border-red-600'>
             <div>
                 <div>
-                    {/* <img src={} alt="image" /> */}
+                    <img src={kspiIcon} alt="KSPI icon" />
                 </div>
             </div>
         </div>
