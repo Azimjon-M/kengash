f8fdc6c7db44b531dcb9200acf04d69e94b6f286
nodirbek27 <nodirjon0927@gmail.com>
2024-01-06 16:19:36 +0500

    update azolar

--- src/components/AddAzoCom/index.jsx
+++ src/components/AddAzoCom/index.jsx
@@ -6,20 +6,22 @@ const AddAzoCom = () => {
         <div>
             <Breadcrumb locationPage="A'zo qo'shish" />
             <div className='p-5'>
-                <h2 className='text-center text-3xl font-bold'>A'zo qo'shish</h2>
+                <h2 className='text-center text-2xl font-bold mb-5'>A'zo qo'shish</h2>
                 <div>
-                    <form className="max-w-sm mx-auto">
+                    <form className="max-w-sm md:max-w-lg lg:max-w-2xl xl:max-w-4xl mx-auto mb-5">
                         <div className="mb-5">
                             <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Foydalanuvchi nomi</label>
                             <input type="text" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Foydalanuvchi nomi" required />
                         </div>
-                        <div className="mb-5">
-                            <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ismi</label>
-                            <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ismi" required />
-                        </div>
-                        <div className="mb-5">
-                            <label htmlFor="surname" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Familiyasi</label>
-                            <input type="text" id="surname" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Familiyasi" required />
+                        <div className='grid lg:grid-cols-2 lg:gap-4'>
+                            <div className="mb-5">
+                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ismi</label>
+                                <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ismi" required />
+                            </div>
+                            <div className="mb-5">
+                                <label htmlFor="surname" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Familiyasi</label>
+                                <input type="text" id="surname" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Familiyasi" required />
+                            </div>
                         </div>
                         <div className="mb-5">
                             <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Huquq</label>
@@ -28,13 +30,15 @@ const AddAzoCom = () => {
                                 <option>A'zo</option>
                             </select>
                         </div>
-                        <div className="mb-5">
-                            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Parol</label>
-                            <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Parol' required />
-                        </div>
-                        <div className="mb-5">
-                            <label htmlFor="confirmPassword" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Parolni tasdiqlash</label>
-                            <input type="password" id="confirmPassword" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Parolni tasdiqlash' required />
+                        <div className='grid lg:grid-cols-2 lg:gap-4'>
+                            <div className="mb-5">
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Parol</label>
+                                <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Parol' required />
+                            </div>
+                            <div className="mb-5">
+                                <label htmlFor="confirmPassword" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Parolni tasdiqlash</label>
+                                <input type="password" id="confirmPassword" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Parolni tasdiqlash' required />
+                            </div>
                         </div>
                         <button type="submit" className='btn bg-[#05B967] hover:bg-[#07b867] text-white w-full'>Qo'shish</button>
                     </form>
