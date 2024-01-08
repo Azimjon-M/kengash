b5a137dd0ca16c16612e559bad05722c764e954b
Azimjon-M <azimeliboev2000@gmail.com>
2024-01-08 15:11:36 +0500

    Takliflar ++

--- src/components/Breadcrumb/index.jsx
+++ src/components/Breadcrumb/index.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Breadcrumb = ({locationPage, locationPageTwo}) => {
     let locPageToLowerCase = locationPage.toLowerCase();
     return (
-        <div className='flex justify-start items-center gap-x-3 bg-[#E9ECEF] px-6 py-3'>
+        <div className='flex justify-start items-center gap-x-3 bg-[#E9ECEF] shadow-lg px-6 py-3'>
             <div>
                 <Link to="/asosiy" className='text-[#05B967]'>
                     Asosiy
--- src/components/TakliflarCom/index.jsx
+++ src/components/TakliflarCom/index.jsx
@@ -1,15 +1,66 @@
-import React from 'react';
-import Breadcrumb from '../Breadcrumb';
+import React from "react";
+import Breadcrumb from "../Breadcrumb";
+import { NavLink } from "react-router-dom";
+import { FaXmark, FaPlus } from "react-icons/fa6";
 
 const TakliflarCom = () => {
+    const data = [
+        {
+            id: 1,
+            taklifNomi: "taklif nomi: 1",
+            nomzod: "Nomzod 1",
+            berilganVaqt: "10",
+        },
+        {
+            id: 2,
+            taklifNomi: "taklif nomi: 2",
+            nomzod: "Nomzod 2",
+            berilganVaqt: "5",
+        },
+        {
+            id: 3,
+            taklifNomi: "taklif nomi: 3",
+            nomzod: "Nomzod 3",
+            berilganVaqt: "3",
+        },
+    ];
     return (
-        <div>
+        <div className="bg-[#F3F7FA]">
             <Breadcrumb locationPage="Takliflar" />
-            <div>
-                Main
+            <div className="flex flex-col gap-y-4 text-black">
+                <div className="text-center text-2xl font-semibold py-6">
+                    Barcha takliflar
+                </div>
+                <div className="flex flex-col items-center gap-y-4 py-4">
+                    <NavLink
+                        to="/takliflar/addTaklif"
+                        className="text-[16px] btn btn-success bg-[#05B967] font-medium text-white px-8"
+                    >
+                        Taklif kiritish <FaPlus />
+                    </NavLink>
+                        
+                    <button
+                        to="/"
+                        className="text-[16px] btn btn-error font-medium text-white px-8"
+                    >
+                        Barcha takliflarni o'chirish <FaXmark />
+                    </button>
+                </div>
+                <div className='text-lg font-semibold text-center'>Kengashga qo'yilmagan takliflar:</div>
+                <div className="flex flex-col items-center">
+                    {data.map((item) => (
+                        <div key={item.id} className="border border-red-500">
+                            <div>Taklif nomi: {item.taklifNomi}</div>
+                            <div>Nomzod: {item.nomzod}</div>
+                            <div>Berilgan vaqt: {item.berilganVaqt}</div>
+                        </div>
+                    ))}
+                </div>
+                <div>Kengashga qo'yilgan takliflar:</div>
+                <div>Card</div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default TakliflarCom;
