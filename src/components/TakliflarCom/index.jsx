d7d1c2f2c3d062f7facfd0a4656f5c8332d225cf
Azimjon-M <azimeliboev2000@gmail.com>
2024-01-09 11:47:00 +0500

    Navabr ++ taklif ++

--- src/components/TakliflarCom/index.jsx
+++ src/components/TakliflarCom/index.jsx
@@ -2,24 +2,26 @@ import React from "react";
 import Breadcrumb from "../Breadcrumb";
 import { NavLink } from "react-router-dom";
 import { FaXmark, FaPlus } from "react-icons/fa6";
+import {BiEditAlt} from 'react-icons/bi';
+import {MdDeleteOutline} from 'react-icons/md';
 
 const TakliflarCom = () => {
     const data = [
         {
             id: 1,
-            taklifNomi: "taklif nomi: 1",
+            taklifNomi: "taklif 1 asdp aikj sdpoa jsdipoja iolhjsdoil aijsdioajosdij",
             nomzod: "Nomzod 1",
             berilganVaqt: "10",
         },
         {
             id: 2,
-            taklifNomi: "taklif nomi: 2",
+            taklifNomi: "taklif 2",
             nomzod: "Nomzod 2",
             berilganVaqt: "5",
         },
         {
             id: 3,
-            taklifNomi: "taklif nomi: 3",
+            taklifNomi: "taklif 3",
             nomzod: "Nomzod 3",
             berilganVaqt: "3",
         },
@@ -33,31 +35,49 @@ const TakliflarCom = () => {
                 </div>
                 <div className="flex flex-col items-center gap-y-4 py-4">
                     <NavLink
-                        to="/takliflar/addTaklif"
-                        className="text-[16px] btn btn-success bg-[#05B967] font-medium text-white px-8"
+                        to="/takliflar/add-taklif"
+                        className="text-[16px] btn btn-success bg-[#05B967] font-medium text-white"
                     >
                         Taklif kiritish <FaPlus />
                     </NavLink>
                         
                     <button
                         to="/"
-                        className="text-[16px] btn btn-error font-medium text-white px-8"
+                        className="text-[16px] btn btn-error font-medium text-white"
                     >
                         Barcha takliflarni o'chirish <FaXmark />
                     </button>
                 </div>
-                <div className='text-lg font-semibold text-center'>Kengashga qo'yilmagan takliflar:</div>
-                <div className="flex flex-col items-center">
+                <div className='text-xl font-semibold text-center mt-4'>Kengashga qo'yilmagan takliflar:</div>
+                <div className="flex flex-col items-center gap-y-4 px-3 overflow-hidden">
                     {data.map((item) => (
-                        <div key={item.id} className="border border-red-500">
-                            <div>Taklif nomi: {item.taklifNomi}</div>
-                            <div>Nomzod: {item.nomzod}</div>
-                            <div>Berilgan vaqt: {item.berilganVaqt}</div>
+                        <div  data-aos="fade-left" key={item.id} className="w-full border bg-white border-gray-500 rounded-md bg-gradient-to-r from-gray-50 to-gray-400 p-2">
+                            <div className="line-clamp-1"><b>Taklif nomi:</b> {item.taklifNomi}</div>
+                            <div><b>Nomzod:</b> {item.nomzod}</div>
+                            <div><b>Berilgan vaqt:</b> {item.berilganVaqt} daqiqa</div>
+                            <div className="flex justify-end">
+                                <button className="btn btn-sm btn-success bg-[#05B967] font-medium text-white mb-4" >Faollashtirish</button>
+                            </div>
+                            <div className="flex justify-end items-center gap-x-2">
+                                <BiEditAlt className="cursor-pointer text-[24px] text-[#05B967]" /> <MdDeleteOutline className="cursor-pointer text-[24px] text-red-600" />
+                            </div>
                         </div>
                     ))}
                 </div>
-                <div>Kengashga qo'yilgan takliflar:</div>
-                <div>Card</div>
+
+                <div className='text-xl font-semibold text-center mt-8'>Kengashga qo'yilgan takliflar:</div>
+                <div className="flex flex-col items-center gap-y-4 px-3 mb-6 overflow-hidden">
+                    {data.map((item) => (
+                        <div  data-aos="fade-right" key={item.id} className="w-full border bg-white border-gray-500 rounded-md bg-gradient-to-r from-green-500 to-green-200 p-2">
+                            <div className="line-clamp-1"><b>Taklif nomi:</b> {item.taklifNomi}</div>
+                            <div><b>Nomzod:</b> {item.nomzod}</div>
+                            <div><b>Berilgan vaqt:</b> {item.berilganVaqt} daqiqa</div>
+                            <div className="flex justify-end">
+                                <button className="btn btn-sm btn-error bg-red-600 font-medium text-white" >Faolsizlashtirish</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
