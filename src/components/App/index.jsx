bfa0c335ca9bd90bc8426765b09467c7afeea6f2
nodirbek27 <nodirjon0927@gmail.com>
2024-01-09 11:49:51 +0500

    update login

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useRoutes, useLocation } from 'react-router-dom';
 import { routes } from '../../routes';
 import './style.css';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
+import Main from '../../pages/Main';
 
 const App = () => {
+
+    const [ token, setToken ] = useState(window.localStorage.getItem('token'))
 
     const location = useLocation();
 
@@ -14,14 +17,22 @@ const App = () => {
     const hideNavbarFooter = noNavLink.includes(location.pathname);
 
     const content = useRoutes(routes);
-    
+
     return (
-        <div> 
-            {!hideNavbarFooter && <Navbar />}
-            {content}
-            {!hideNavbarFooter && <Footer />}
-            
-        </div>
+        <div>
+            {
+                token ? (
+                    <Main />
+                ) : (
+                    < div >
+                        {!hideNavbarFooter && <Navbar />}
+                        {content}
+                        {!hideNavbarFooter && <Footer />}
+                    </div>
+                )
+
+            }
+        </div >
     )
 }
 
