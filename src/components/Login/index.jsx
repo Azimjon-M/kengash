b18ca2d78ff631924b4ea3301cc2b167f4e45ddd
Azimjon-M <azimeliboev2000@gmail.com>
2024-01-10 15:58:43 +0500

    login ++

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,50 +1,53 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import logo from '../../assets/img/logo_kspi.png'
+import React from "react";
+// import { useNavigate } from "react-router-dom";
+import logo from "../../assets/img/logo_kspi.png";
+import { useFormik } from "formik";
+import axios from "axios";
 
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
+    // const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    const handleLogin = async (e) => {
-        e.preventDefault()
-        const userData = { username, email, password };
-        // console.log( {username, email, password} );
-        try {
-                fetch('http://192.168.0.29/api/v1/dj-rest-auth/login/', {
+    const formik = useFormik({
+        initialValues: {
+            username: "",
+            email: "",
+            password: "",
+        },
+        onSubmit: (values) => {
+            axios({
                 method: 'POST',
-                mode: 'no-cors',
-                headers: {
-                    // 'accept': 'application/json',
-                    // 'Content-Type': 'application/json',
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: JSON.stringify({username, email, password})
-            }).then(res => console.log(res)).catch(err => console.log(err))
+                url: 'http://192.168.0.29/api/v1/dj-rest-auth/login/',
+                data: values
+            })
+            .then(function (res) {
+                console.log(res)
+                alert('Successfully signed up!');  
+            })
+            .catch(function (res) {
+                console.log(res)
+            });
 
-            // if (response.ok) {
-            //     const data = await response.json();
-            //     console.log(data);
-            //     const token = data.token;
-            //     localStorage.setItem('token', token);
-            //     navigate('/asosiy');
-            // }
-            // else {
-            //     alert('Foydalanuvchi nomi yoki parol xato!');
-            // }
-        } catch (error) {
-            console.error('Xatolik:', error);
-        }
-    };
+            //http://192.168.0.29/api/v1/dj-rest-auth/login/
+
+            // fetch("http://192.168.0.29/api/v1/dj-rest-auth/login/", {
+            //     method: "POST",
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //         'accept': 'application/json'
+            //     },
+            //     body: JSON.stringify(values)
+            // }).then((res) => res.json()).then((data) => console.log(data)).catch((err) => console.log(err));
+
+            // then(res => ( res.json())).
+            // alert(JSON.stringify(values, null, 2));
+        },
+    });
 
     return (
         <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
             <div className="w-full p-6 m-auto bg-white rounded-md shadow-md max-w-xs sm:max-w-sm lg:max-w-lg">
-                <div className='flex justify-center'>
-                    <img className='w-[80px]' src={logo} alt="" />
+                <div className="flex justify-center">
+                    <img className="w-[80px]" src={logo} alt="" />
                 </div>
                 <h1 className="text-xl font-bold text-center text-success mb-2">
                     Qo'qon davlat pedagogika instituti
@@ -52,27 +55,50 @@ const Login = () => {
                 <h2 className="text-lg font-semibold text-center text-black">
                     Institut ichki kengashiga kirish
                 </h2>
-                <form onSubmit={handleLogin} className="mt-6" id='loginForm'>
+                <form
+                    onSubmit={formik.handleSubmit}
+                    className="mt-6"
+                    id="loginForm"
+                >
                     <div className="mb-2">
-                        <label htmlFor="username" className="block text-sm font-semibold text-gray-800">
+                        <label
+                            htmlFor="username"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
                             Foydalanuvchi nomi
                         </label>
-                        <input type="username" id='username' name='username' value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Foydalanuvchi nomi' className="block w-full px-4 py-2 mt-2 text-black bg-white border rounded-md focus:border-[#28a745] focus:ring-[#25a620] focus:outline-none focus:ring focus:ring-opacity-40" />
+                        <input
+                            type="username"
+                            id="username"
+                            name="username"
+                            onChange={formik.handleChange}
+                            value={formik.values.username}
+                            placeholder="Foydalanuvchi nomi"
+                            className="block w-full px-4 py-2 mt-2 text-black bg-white border rounded-md focus:border-[#28a745] focus:ring-[#25a620] focus:outline-none focus:ring focus:ring-opacity-40"
+                        />
                     </div>
                     <div className="mb-2">
-                        <label htmlFor="email" className="block text-sm font-semibold text-gray-800">
-                            Email
-                        </label>
-                        <input type="email" id='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' className="block w-full px-4 py-2 mt-2 text-black bg-white border rounded-md focus:border-[#28a745] focus:ring-[#25a620] focus:outline-none focus:ring focus:ring-opacity-40" />
-                    </div>
-                    <div className="mb-2">
-                        <label htmlFor="password" className="block text-sm font-semibold text-gray-800">
+                        <label
+                            htmlFor="password"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
                             Parol
                         </label>
-                        <input type="password" id='password' name='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Parol' className="block w-full px-4 py-2 mt-2 text-black bg-white border rounded-md focus:border-[#28a745] focus:ring-[#25a620] focus:outline-none focus:ring focus:ring-opacity-40" />
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            onChange={formik.handleChange}
+                            value={formik.values.password}
+                            placeholder="Parol"
+                            className="block w-full px-4 py-2 mt-2 text-black bg-white border rounded-md focus:border-[#28a745] focus:ring-[#25a620] focus:outline-none focus:ring focus:ring-opacity-40"
+                        />
                     </div>
                     <div className="mt-6">
-                        <button type='submit' className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-[#28a745] rounded-md hover:bg-[#25a620] focus:outline-none">
+                        <button
+                            type="submit"
+                            className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-[#28a745] rounded-md hover:bg-[#25a620] focus:outline-none"
+                        >
                             Kirish
                         </button>
                     </div>
@@ -80,6 +106,6 @@ const Login = () => {
             </div>
         </div>
     );
-}
+};
 
 export default Login;
