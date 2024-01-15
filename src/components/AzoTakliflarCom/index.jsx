82f404d49ffdc26602b995bc6f8eb216798ea73a
nodirbek27 <nodirjon0927@gmail.com>
2024-01-15 13:13:06 +0500

    azotakliflarcom ++

--- src/components/AzoTakliflarCom/index.jsx
+++ src/components/AzoTakliflarCom/index.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const AzoTakliflarCom = () => {
+
+    const apiUrl = 'https://kengash.pythonanywhere.com/api/v1/taklif/';
+    const token = '6ce7e827abb14f77b14015d0dd778b0fef76b53e';
+
+    const [data, setData] = useState([]);
+    console.log(data);
+
+    useEffect(() => {
+        fetch(apiUrl, {
+            headers: {
+                'Authorization': `Token ${token}`,
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => console.error('Xatolik:', error));
+    }, []);
+    // console.log(Date.now());
+
     return (
-        <div>AzoTakliflarCom</div>
+        <div className='bg-[#F3F7FA] min-h-[calc(100vh-125px)] px-2'>
+            <div className='md:p-8'>
+                <h2 className='text-center text-2xl xl:text-3xl font-semibold mb-5'>Takliflar</h2>
+                <Link>
+                    <div>
+                        {
+                            data.map((taklif, idx) => {
+                                return (
+                                    <div key={idx}>
+                                        {taklif.bitalik_taklif ?
+                                        <div className='border-2 rounded p-2 lg:px-8 mb-5 shadow-md'>
+                                            {/* VAQT */}
+                                            <div className='flex items-center justify-between mb-5'>
+                                                <h2 className='text-2xl font-semibold'>Berilgan vaqt</h2>
+                                                <span className="countdown font-mono text-2xl font-bold">
+                                                    <span style={{ "--value": 24 }}></span>:
+                                                    <span style={{ "--value": 28 }}></span>
+                                                </span>
+                                            </div>
+                                            {/* TAKLIF */}
+                                            <div className='mb-5 md:text-lg xl:text-xl'><b>Taklif: </b>{taklif.name}</div>
+                                            {/* NOMZOD */}
+                                            <div className='mb-5 md:text-lg xl:text-xl'><b>Nomzod: </b>{taklif.nomzod}</div>
+                                            {/* BUTTONS */}
+                                            <div className='flex items-center justify-between md:justify-end'>
+                                                <button className='btn btn-sm md:btn md:text-white rounded bg-green-600 md:bg-green-600 w-[100px] md:w-[120px] lg:text-lg md:mx-3 text-white'>Roziman</button>
+                                                <button className='btn btn-sm md:btn md:text-white rounded bg-red-600 md:bg-red-600 w-[100px] md:w-[120px] lg:text-lg md:mx-3 text-white'>Qarshiman</button>
+                                                <button className='btn btn-sm md:btn md:text-white rounded bg-yellow-600 md:bg-yellow-600 w-[100px] md:w-[120px] lg:text-lg md:ml-3 text-white'>Betarafman</button>
+                                            </div>
+                                        </div>
+                                        :
+                                        <div className='border-2 rounded p-2 lg:px-8 mb-5 shadow-md'>
+                                            {/* VAQT */}
+                                            <div className='flex items-center justify-between mb-5'>
+                                                <h2 className='text-2xl font-semibold'>Berilgan vaqt</h2>
+                                                <span className="countdown font-mono text-2xl font-bold">
+                                                    <span style={{ "--value": 24 }}></span>:
+                                                    <span style={{ "--value": 28 }}></span>
+                                                </span>
+                                            </div>
+                                            {/* TAKLIF */}
+                                            <div className='mb-5 md:text-lg xl:text-xl'><b>Taklif: </b>{taklif.name}</div>
+                                            {/* NOMZOD */}
+                                            <div className='mb-5 md:text-lg xl:text-xl'><b>Nomzod: </b>{taklif.nomzod}</div>
+                                            <div className='mb-5 md:text-lg xl:text-xl'><b>Nomzod: </b>{taklif.nomzod1}</div>
+                                            <div className='mb-5 md:text-lg xl:text-xl'><b>Nomzod: </b>{taklif.nomzod2}</div>
+                                            <div className='mb-5 md:text-lg xl:text-xl'><b>Nomzod: </b>{taklif.nomzod3}</div>
+                                        </div>
+                                    }
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </Link>
+            </div>
+        </div>
     )
 }
 
-export default AzoTakliflarCom
+export default AzoTakliflarCom;
