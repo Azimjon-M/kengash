6930811b3be91beb61525a44405f126c18d096d6
nodirbek27 <nodirjon0927@gmail.com>
2024-01-17 13:55:45 +0500

    change justify in azoTakliflarCom

--- src/components/AzoTakliflarCom/index.jsx
+++ src/components/AzoTakliflarCom/index.jsx
@@ -101,10 +101,10 @@ const AzoTakliflarCom = () => {
                                             {/* NOMZOD */}
                                             <div className='mb-5 md:text-lg xl:text-xl'><b>Nomzod: </b>{taklif.nomzod}</div>
                                             {/* BUTTONS */}
-                                            <div className='flex items-center justify-between md:justify-end'>
-                                                <button onClick={() => handleVote(taklif, '1')} className='btn btn-sm md:btn md:text-white rounded bg-green-600 hover:bg-green-500 md:bg-green-600 w-[100px] md:w-[120px] lg:text-lg md:mx-3 text-white'>Roziman</button>
-                                                <button onClick={() => handleVote(taklif, '2')} className='btn btn-sm md:btn md:text-white rounded bg-red-600 hover:bg-red-500 md:bg-red-600 w-[100px] md:w-[120px] lg:text-lg md:mx-3 text-white'>Qarshiman</button>
-                                                <button onClick={() => handleVote(taklif, '3')} className='btn btn-sm md:btn md:text-white rounded bg-yellow-600 hover:bg-yellow-500 md:bg-yellow-600 w-[100px] md:w-[120px] lg:text-lg md:ml-3 text-white'>Betarafman</button>
+                                            <div className='flex items-center justify-between md:justify-end md:gap-3'>
+                                                <button onClick={() => handleVote(taklif, '1')} className='btn btn-sm md:btn md:text-white rounded bg-green-600 hover:bg-green-500 md:bg-green-600 w-[100px] md:w-[120px] lg:text-lg text-white'>Roziman</button>
+                                                <button onClick={() => handleVote(taklif, '2')} className='btn btn-sm md:btn md:text-white rounded bg-red-600 hover:bg-red-500 md:bg-red-600 w-[100px] md:w-[120px] lg:text-lg text-white'>Qarshiman</button>
+                                                <button onClick={() => handleVote(taklif, '3')} className='btn btn-sm md:btn md:text-white rounded bg-yellow-600 hover:bg-yellow-500 md:bg-yellow-600 w-[100px] md:w-[120px] lg:text-lg text-white'>Betarafman</button>
                                             </div>
                                         </div>
                                         :
@@ -120,8 +120,7 @@ const AzoTakliflarCom = () => {
                                             {/* TAKLIF */}
                                             <div className='mb-5 md:text-lg xl:text-xl'><b>Taklif: </b>{taklif.name}</div>
                                             {/* NOMZOD */}
-                                            <h3 className='text-2xl font-semibold text-center mb-5'>Nomzodlar</h3>
-                                            <div className='text-center md:flex md:items-center md:justify-evenly md:flex-wrap'>
+                                            <div className='text-center md:flex md:items-center md:justify-end md:flex-wrap md:gap-3'>
                                                 <div className='mb-5 md:text-lg xl:text-xl'><button className='btn bg-blue-600 hover:bg-blue-500 text-white'>{taklif.nomzod}</button></div>
                                                 <div className='mb-5 md:text-lg xl:text-xl'><button className='btn bg-blue-600 hover:bg-blue-500 text-white'>{taklif.nomzod1}</button></div>
                                                 <div className={`${taklif.nomzod2 ? "" : "hidden"} mb-5 md:text-lg xl:text-xl`}><button className='btn bg-blue-600 hover:bg-blue-500 text-white'>{taklif.nomzod2}</button></div>
