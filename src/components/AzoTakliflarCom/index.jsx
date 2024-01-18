68bcfc37656885106c42c29ca5ae01f1c159e5f1
nodirbek27 <nodirjon0927@gmail.com>
2024-01-18 12:07:15 +0500

    azoTakliflar++

--- src/components/AzoTakliflarCom/index.jsx
+++ src/components/AzoTakliflarCom/index.jsx
@@ -21,33 +21,36 @@ const AzoTakliflarCom = () => {
             .then((data) => setData(data))
             .catch((error) => console.error("Xatolik:", error));
     };
-    const getTime = () => {
-        data.map((item) => {
-            //TUGASH VAQTI
-            let vaqt = item.tugash_vaqti.split(":").map(Number);
-            let now = new Date()
-            // QOLGAN VAQT
-            let qolganVaqt = (vaqt[0] * 60 * 60 + vaqt[1] * 60) - (now.getHours() * 60 * 60 + now.getMinutes() * 60 + now.getSeconds());
-            let qolganMinut = Math.floor(qolganVaqt / 60)
-            let qolganSeconds = (qolganVaqt % 60)
-            console.log(qolganMinut, qolganSeconds);
-            return (qolganMinut, qolganSeconds);
-        })
-    };
+
     useEffect(() => {
         GetDataFromAPI();
-        const interval = setInterval(() => getTime(), 1000);
-        return () => clearInterval(interval);
     }, []);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setData(data => data.map(item => {
+                let vaqt = item.tugash_vaqti.split(":").map(Number);
+                let now = new Date();
+                let qolganVaqt = (vaqt[0] * 60 * 60 + vaqt[1] * 60) - (now.getHours() * 60 * 60 + now.getMinutes() * 60 + now.getSeconds());
+                let qolganMinut = Math.floor(qolganVaqt / 60);
+                let qolganSeconds = qolganVaqt % 60;
+
+                return { ...item, qolganMinut, qolganSeconds };
+            }));
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [data]);
+
 
 
     // POST DATA
     const handleVote = ({ id, name, bitalik_taklif }, action) => {
+        const user_id = localStorage.getItem('user_id');
         let postData = {
             taklif_id: id,
             name: name,
             bitalik_taklif: bitalik_taklif,
-            user_id: id,
+            user_id: user_id,
         };
 
         switch (action) {
@@ -159,47 +162,79 @@ const AzoTakliflarCom = () => {
                                     {(taklif.yoqish) &&
                                         (
                                             (taklif.bitalik_taklif) ?
-                                                <div className='border-2 rounded p-2 lg:px-8 mb-5 shadow-md'>
-                                                    {/* VAQT */}
-                                                    <div className='flex items-center justify-between mb-5'>
-                                                        <h2 className='text-2xl font-semibold'>Berilgan vaqt</h2>
-                                                        <span className="countdown font-mono text-2xl font-bold">
-                                                            <span style={{ "--value": 24 }}></span>:
-                                                            <span style={{ "--value": 28 }}></span>
-                                                        </span>
+                                                (taklif.qolganMinut <= 0 && taklif.qolganSeconds <= 0) ? null : (
+                                                    <div className='border-2 rounded p-2 lg:px-8 mb-5 shadow-md'>
+                                                        {/* VAQT */}
+                                                        <div className='flex items-center justify-between mb-5'>
+                                                            <h2 className='text-2xl font-semibold'>Berilgan vaqt</h2>
+                                                            {taklif.qolganMinut <= 0 && taklif.qolganSeconds <= 0 ? (
+                                                                <span className="countdown font-mono text-2xl font-bold text-red-500">00:00</span>
+                                                            ) : (
+                                                                <span className="countdown font-mono text-2xl font-bold">
+                                                                    <span style={{ "--value": taklif.qolganMinut }}></span>:
+                                                                    <span style={{ "--value": taklif.qolganSeconds }}></span>
+                                                                </span>
+                                                            )}
+                                                        </div>
+                                                        {/* TAKLIF */}
+                                                        <div className='mb-5 md:text-lg xl:text-xl'><b>Taklif: </b>{taklif.name}</div>
+                                                        {/* NOMZOD */}
+                                                        <div className='mb-5 md:text-lg xl:text-xl'><b>Nomzod: </b>{taklif.nomzod}</div>
+                                                        {/* BUTTONS */}
+                                                        <div className='flex items-center justify-between md:justify-end md:gap-3'>
+                                                            <button
+                                                                onClick={() => handleVote(taklif, '1')}
+                                                                className={`btn btn-sm md:btn md:text-white rounded ${taklif.qolganMinut <= 0 && taklif.qolganSeconds <= 0 ? 'bg-gray-300 cursor-not-allowed' : 'bg-green-600 hover:bg-green-500 md:bg-green-600'
+                                                                    } w-[100px] md:w-[120px] lg:text-lg text-white`}
+                                                                disabled={taklif.qolganMinut <= 0 && taklif.qolganSeconds <= 0}
+                                                            >
+                                                                Roziman
+                                                            </button>
+                                                            <button
+                                                                onClick={() => handleVote(taklif, '2')}
+                                                                className={`btn btn-sm md:btn md:text-white rounded ${taklif.qolganMinut <= 0 && taklif.qolganSeconds <= 0 ? 'bg-gray-300 cursor-not-allowed' : 'bg-red-600 hover:bg-red-500 md:bg-red-600'
+                                                                    } w-[100px] md:w-[120px] lg:text-lg text-white`}
+                                                                disabled={taklif.qolganMinut <= 0 && taklif.qolganSeconds <= 0}
+                                                            >
+                                                                Qarshiman
+                                                            </button>
+                                                            <button
+                                                                onClick={() => handleVote(taklif, '3')}
+                                                                className={`btn btn-sm md:btn md:text-white rounded ${taklif.qolganMinut <= 0 && taklif.qolganSeconds <= 0 ? 'bg-gray-300 cursor-not-allowed' : 'bg-yellow-600 hover:bg-yellow-500 md:bg-yellow-600'
+                                                                    } w-[100px] md:w-[120px] lg:text-lg text-white`}
+                                                                disabled={taklif.qolganMinut <= 0 && taklif.qolganSeconds <= 0}
+                                                            >
+                                                                Betarafman
+                                                            </button>
+                                                        </div>
                                                     </div>
-                                                    {/* TAKLIF */}
-                                                    <div className='mb-5 md:text-lg xl:text-xl'><b>Taklif: </b>{taklif.name}</div>
-                                                    {/* NOMZOD */}
-                                                    <div className='mb-5 md:text-lg xl:text-xl'><b>Nomzod: </b>{taklif.nomzod}</div>
-                                                    {/* BUTTONS */}
-                                                    <div className='flex items-center justify-between md:justify-end md:gap-3'>
-                                                        <button onClick={() => handleVote(taklif, '1')} className='btn btn-sm md:btn md:text-white rounded bg-green-600 hover:bg-green-500 md:bg-green-600 w-[100px] md:w-[120px] lg:text-lg text-white'>Roziman</button>
-                                                        <button onClick={() => handleVote(taklif, '2')} className='btn btn-sm md:btn md:text-white rounded bg-red-600 hover:bg-red-500 md:bg-red-600 w-[100px] md:w-[120px] lg:text-lg text-white'>Qarshiman</button>
-                                                        <button onClick={() => handleVote(taklif, '3')} className='btn btn-sm md:btn md:text-white rounded bg-yellow-600 hover:bg-yellow-500 md:bg-yellow-600 w-[100px] md:w-[120px] lg:text-lg text-white'>Betarafman</button>
-                                                    </div>
-                                                </div>
+                                                )
                                                 :
-                                                <div className='border-2 rounded p-2 lg:px-8 mb-5 shadow-md'>
-                                                    {/* VAQT */}
-                                                    <div className='flex items-center justify-between mb-5'>
-                                                        <h2 className='text-2xl font-semibold'>Berilgan vaqt</h2>
-                                                        <span className="countdown font-mono text-2xl font-bold">
-                                                            <span style={{ "--value": 24 }}></span>:
-                                                            <span style={{ "--value": 28 }}></span>
-                                                        </span>
+                                                (taklif.qolganMinut <= 0 && taklif.qolganSeconds <= 0) ? null : (
+                                                    <div className='border-2 rounded p-2 lg:px-8 mb-5 shadow-md'>
+                                                        {/* VAQT */}
+                                                        <div className='flex items-center justify-between mb-5'>
+                                                            <h2 className='text-2xl font-semibold'>Berilgan vaqt</h2>
+                                                            {taklif.qolganMinut <= 0 && taklif.qolganSeconds <= 0 ? (
+                                                                <span className="countdown font-mono text-2xl font-bold text-red-500">00:00</span>
+                                                            ) : (
+                                                                <span className="countdown font-mono text-2xl font-bold">
+                                                                    <span style={{ "--value": taklif.qolganMinut }}></span>:
+                                                                    <span style={{ "--value": taklif.qolganSeconds }}></span>
+                                                                </span>
+                                                            )}
+                                                        </div>
+                                                        {/* TAKLIF */}
+                                                        <div className='mb-5 md:text-lg xl:text-xl'><b>Taklif: </b>{taklif.name}</div>
+                                                        {/* NOMZODLAR */}
+                                                        <div className='text-center md:flex md:items-center md:justify-end gap-3 md:flex-wrap'>
+                                                            <div className='mb-5 md:text-lg xl:text-xl'><button onClick={() => handleNomzodVote(taklif, '1')} className='btn bg-blue-600 hover:bg-blue-500 text-white' disabled={taklif.qolganMinut <= 0 && taklif.qolganSeconds <= 0}>{taklif.nomzod}</button></div>
+                                                            <div className='mb-5 md:text-lg xl:text-xl'><button onClick={() => handleNomzodVote(taklif, '2')} className='btn bg-blue-600 hover:bg-blue-500 text-white' disabled={taklif.qolganMinut <= 0 && taklif.qolganSeconds <= 0}>{taklif.nomzod1}</button></div>
+                                                            <div className={`${taklif.nomzod2 ? "" : "hidden"} mb-5 md:text-lg xl:text-xl`}><button onClick={() => handleNomzodVote(taklif, '3')} className='btn bg-blue-600 hover:bg-blue-500 text-white' disabled={taklif.qolganMinut <= 0 && taklif.qolganSeconds <= 0}>{taklif.nomzod2}</button></div>
+                                                            <div className={`${taklif.nomzod3 ? "" : "hidden"} mb-5 md:text-lg xl:text-xl`}><button onClick={() => handleNomzodVote(taklif, '4')} className='btn bg-blue-600 hover:bg-blue-500 text-white' disabled={taklif.qolganMinut <= 0 && taklif.qolganSeconds <= 0}>{taklif.nomzod3}</button></div>
+                                                        </div>
                                                     </div>
-                                                    {/* TAKLIF */}
-                                                    <div className='mb-5 md:text-lg xl:text-xl'><b>Taklif: </b>{taklif.name}</div>
-                                                    {/* NOMZODLAR */}
-                                                    <div className='text-center md:flex md:items-center md:justify-end gap-3 md:flex-wrap'>
-
-                                                        <div className='mb-5 md:text-lg xl:text-xl'><button onClick={() => handleNomzodVote(taklif, '1')} className='btn bg-blue-600 hover:bg-blue-500 text-white'>{taklif.nomzod}</button></div>
-                                                        <div className='mb-5 md:text-lg xl:text-xl'><button onClick={() => handleNomzodVote(taklif, '2')} className='btn bg-blue-600 hover:bg-blue-500 text-white'>{taklif.nomzod1}</button></div>
-                                                        <div className={`${taklif.nomzod2 ? "" : "hidden"} mb-5 md:text-lg xl:text-xl`}><button onClick={() => handleNomzodVote(taklif, '3')} className='btn bg-blue-600 hover:bg-blue-500 text-white'>{taklif.nomzod2}</button></div>
-                                                        <div className={`${taklif.nomzod3 ? "" : "hidden"} mb-5 md:text-lg xl:text-xl`}><button onClick={() => handleNomzodVote(taklif, '4')} className='btn bg-blue-600 hover:bg-blue-500 text-white'>{taklif.nomzod3}</button></div>
-                                                    </div>
-                                                </div>
+                                                )
                                         )
                                     }
                                 </div>
