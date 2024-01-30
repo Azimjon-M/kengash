0e48463a33923755c8641e35c5862f3be57e9a07
nodirbek27 <nodirjon0927@gmail.com>
2024-01-30 11:20:35 +0500

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://192.168.0.29/api/v1/',
+    baseURL: 'http://192.168.0.29/api/v1/',
     headers: {
         "Content-Type": "application/json"
     }
